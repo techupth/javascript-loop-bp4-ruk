b327511ffdded703bc85d214bb56195b61c3c57f
rukrmy007 <38137020+rukrmy007@users.noreply.github.com>
2024-06-15 01:18:20 +0700

    Update ex-1.js

--- ex-1.js
+++ ex-1.js
@@ -2,7 +2,7 @@
 let scores = [89, 70, 60, 50, 100, 71];
 
 // Start coding here
-let deductPoints = 10
+let deductPoints = 10;
 
 for (let number of scores) {
     console.log(number - deductPoints);
